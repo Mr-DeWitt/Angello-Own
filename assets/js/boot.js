22baa63974b147383b2486c6047e79c893d25bdb
szilard.l.fodor@gmail.com <Almafa42Szilvi>
2016-01-25 21:16:01 +0100

    Headjs az érezhetően megbízhatóbban működő LABjs-re cserélve.

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -1,36 +1,37 @@
-head.js(
-    {file: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.js'},
-    {file: '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular.js'},
-    {file: '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular-animate.min.js'},
-    {file: '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular-route.min.js'},
-    {file: '//cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js'},
-    {file: '/vendor/sugar.min.js'},
-    {file: '/vendor/flot/jquery.flot.min.js'},
-    {file: '/vendor/flot/jquery.flot.categories.min.js'},
+$LAB
+    .script('//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.js')
+    .script('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular.js')
+    .script('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular-animate.min.js')
+    .script('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular-route.min.js')
+    .script('//cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js')
+    .script('/vendor/sugar.min.js')
+    .script('/vendor/flot/jquery.flot.min.js')
+    .script('/vendor/flot/jquery.flot.categories.min.js')
 
-    {file: '/src/app/App.js'},
-    {file: '/src/app/service/EndpointConfigService.js'},
-    {file: '/src/app/service/UtilsService.js'},
-    {file: '/src/app/service/LoadingService.js'},
-    {file: '/src/app/model/StoriesModel.js'},
-    {file: '/src/app/model/UsersModel.js'},
-    {file: '/src/app/animation/DetailsAnimation.js'},
-    {file: '/src/app/animation/ListAreaAnimation.js'},
+    .script('/src/app/App.js')
+    .script('/src/app/service/EndpointConfigService.js')
+    .script('/src/app/service/UtilsService.js')
+    .script('/src/app/service/LoadingService.js')
+    .script('/src/app/model/StoriesModel.js')
+    .script('/src/app/model/UsersModel.js')
+    .script('/src/app/animation/DetailsAnimation.js')
+    .script('/src/app/animation/ListAreaAnimation.js')
 
-    {file: '/src/user/User.js'},
-    {file: '/src/user/directives/UserStoryDirective.js'},
+    .script('/src/user/User.js')
+    .script('/src/user/directives/UserStoryDirective.js')
 
-    {file: '/src/storyboard/Storyboard.js'},
-    {file: '/src/storyboard/directives/story.js'},
-    {file: '/src/storyboard/directives/DragAndDrop.js'},
-    {file: '/src/storyboard/controller/StoryboardCtrl.js'},
+    .script('/src/storyboard/Storyboard.js')
+    .script('/src/storyboard/directives/story.js')
+    .script('/src/storyboard/directives/DragAndDrop.js')
+    .script('/src/storyboard/controller/StoryboardCtrl.js')
 
-    {apps: '/src/dashboard/Dashboard.js'},
-    {apps: '/src/dashboard/controller/DashboardController.js'},
-    {file: '/src/dashboard/directive/ChartDirective.js'},
+    .script('/src/dashboard/Dashboard.js')
+    .script('/src/dashboard/controller/DashboardController.js')
+    .script('/src/dashboard/directive/ChartDirective.js')
 
-    {file: '/src/Angello.js'}
-).ready(
-    function () {
+    .script('/src/Angello.js')
+
+    .wait(function () {
         angular.bootstrap(document, ['Angello']);
     });
+
